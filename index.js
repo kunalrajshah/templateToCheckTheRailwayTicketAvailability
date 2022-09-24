8f995078d9fb990b5af7e2f053ebb5cca52f700e
kunal raj shah <rajkunal7279@gmail.com>
2022-09-24 21:47:23 +0530

    Deploy phase 2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 const Train = require("./models/trains");
 const cors = require("cors");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,'/clients/build')));
 dotenv.config();
 app.use(express.json());
 app.use(cors());
@@ -15,6 +18,10 @@ mongoose.connect(process.env.MONGO_URI, (err) => {
       console.log(err);
     }
   });
+
+app.use((req,res,next)=>{
+    res.sendFile(path.resolve(__dirname,"clients","build","index.html"))
+})
 
 app.post("/addTrain", (req,res)=>{
 
@@ -63,7 +70,7 @@ app.post("/search", async (req, res)=>{
 app.get("/", (req, res)=>{
     res.json({"message": "This is railway app"});
 })
-
-app.listen(5000, () => {
+const PORT = process.env.PORT||5000;
+app.listen(PORT, () => {
     console.log("Server started at port 5000");
 });
